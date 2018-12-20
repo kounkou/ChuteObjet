c025fab140c18bd4a781c2d9f994679c6c101097
Jacques <gkounkou@gmail.com>
2018-12-20 11:07:46 -0800

    Update Counter.cpp

--- Counter.cpp
+++ Counter.cpp
@@ -3,7 +3,7 @@
  * 
  * brief            
  *    This application is the result of the collaboration between
- *    Rudy NGoteni, Boungou Jerome and myself
+ *    Rudy NGoteni, Boungou Jerome and Kounkou Godrain Jacques
  *    The purpose was to produce a simulation for a ball falling from
  *    a specific height.
  *    The test contains all kind of ballistic formulas used to reproduce
